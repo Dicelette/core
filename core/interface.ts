d25649c93c443ff254e14c1636e1885a3230bc09
Mara <mara-li@outlook.fr>
2024-03-28 20:08:13 +0100

    implement core

---
+++ core/interface.ts
@@ -0,0 +1,71 @@
+export interface Resultat {
+	dice: string;
+	result: string;
+	comment?: string;
+	compare?: Compare | undefined;
+	modifier?: Modifier;
+}
+
+
+export interface Compare {
+	sign: "<" | ">" | ">=" | "<=" | "=" | "!=" | "==";
+	value: number;
+}
+
+export type Sign = "+" | "-" | "*" | "/" | "%" | "^" | "**";
+
+export interface Modifier {
+	sign?: Sign;
+	value: number;
+}
+
+export type Statistic = {
+	[name: string] : {
+		max?: number;
+		min?: number;
+		combinaison?: string;
+	}
+}
+
+/**
+ * @example
+ * diceType: 1d20
+ * comparator: {
+ * 	sign: ">="
+ * 	value: 20
+ * 	formula: +$
+ * }
+ * The dice throw will be 1d20 + statistique that must be >= 20
+ * @example
+ * diceType: 1d20
+ * comparator: {
+ * 	sign: "<="
+ * }
+ * The dice throw will be 1d20 that must be <= statistique
+ */
+export interface StatisticalTemplate {
+	/** Allow to force the user to choose a name for them characters */
+	charName?: boolean 
+	statistics?: Statistic
+	/**
+	 * A total can be set, it allows to calculate the total value of a future register member
+	 * If the sum of the value > total, the bot will send a message to the user to inform him that the total is exceeded and an error will be thrown
+	 * @note statistique that have a formula will be ignored from the total
+	 */
+	total?: number
+	/** A dice type in the notation supported by the bot */
+	diceType?: string;
+	/**
+	 * How the success/echec will be done 
+	 */
+	critical?: Critical;
+	/** Special dice for damage */
+	damage?: {
+		[name: string]: string;
+	}
+}
+
+export interface Critical {
+		success?: number,
+		failure?: number,
+}
---
+++ core/utils.ts
@@ -0,0 +1,65 @@
+import { evaluate } from "mathjs";
+import removeAccents from "remove-accents";
+
+/**
+ * Escape regex string
+ * @param string {string}
+ */
+export function escapeRegex(string: string) {
+	return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+
+/**
+ * Replace the stat name by their value using stat and after evaluate any formula using `replaceFormulaInDice`
+ * @param originalDice {dice}
+ * @param stats {[name: string]: number}
+ */
+export function generateStatsDice(originalDice: string, stats?: {[name: string]: number}) {
+	let dice = originalDice;
+	if (stats && Object.keys(stats).length > 0) {
+		//damage field support adding statistic, like : 1d6 + strength
+		//check if the value contains a statistic & calculate if it's okay
+		//the dice will be converted before roll 
+		const allStats = Object.keys(stats);
+		for (const stat of allStats) {
+			const regex = new RegExp(escapeRegex(removeAccents(stat)), "gi");
+			if (dice.match(regex)) {
+				const statValue = stats[stat];
+				dice = dice.replace(regex, statValue.toString());
+			}
+		}
+	}
+	return replaceFormulaInDice(dice);
+	
+}
+
+/**
+ * Replace the {{}} in the dice string and evaluate the interior if any
+ * @param dice {string}
+ */
+export function replaceFormulaInDice(dice: string) {
+	const formula = /(?<formula>\{{2}(.+?)\}{2})/gmi;
+	const formulaMatch = formula.exec(dice);
+	if (formulaMatch?.groups?.formula) {
+		const formula = formulaMatch.groups.formula.replaceAll("{{", "").replaceAll("}}", "");
+		try {
+			const result = evaluate(formula);
+			return cleanedDice(dice.replace(formulaMatch.groups.formula, result.toString()));
+		} catch (error) {
+			throw new Error(`[error.invalidFormula, common.space]: ${formulaMatch.groups.formula}`);
+		}
+	}
+	return cleanedDice(dice);
+}
+
+/**
+ * Replace the ++ +- -- by their proper value:
+ * - `++` = `+`
+ * - `+-` = `-`
+ * - `--` = `+`
+ * @param dice {string}
+ */
+export function cleanedDice(dice: string) {
+	return dice.replaceAll("+-", "-").replaceAll("--", "+").replaceAll("++", "+");
+}
