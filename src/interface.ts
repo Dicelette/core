a6be9958bf4ecdae34c45ac1bef3352d3b2610b5
Mara <mara-li@outlook.fr>
2024-06-17 21:53:09 +0200

    docs: add comments in interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,27 +1,70 @@
 export interface Resultat {
+	/**
+	 * Original dice throw
+	 */
 	dice: string;
+	/**
+	 * Result of the dice throw
+	 */
 	result: string;
+	/**
+	 * The comment that was added to the dice throw (if any)
+	 */
 	comment?: string;
+	/**
+	 * The comparison made on the dice
+	 */
 	compare?: Compare | undefined;
+	/**
+	 * If any value was added to the dice throw
+	 */
 	modifier?: Modifier;
 }
 
 export interface Compare {
+	/**
+	 * Sign of the comparison
+	 */
 	sign: "<" | ">" | ">=" | "<=" | "=" | "!=" | "==";
+	/**
+	 * Value of the comparison
+	 */
 	value: number;
 }
 
+/**
+ * Sign format for calculation of modifier
+ */
 export type Sign = "+" | "-" | "*" | "/" | "%" | "^" | "**";
 
 export interface Modifier {
+	/**
+	 * Sign of the modifier
+	 */
 	sign?: Sign;
+	/**
+	 * Value of the modifier
+	 */
 	value: number;
 }
 
+/**
+ * Statistic object template
+ */
 export type Statistic = {
 	[name: string]: {
+		/**
+		 * The value of the statistic that can take the stats
+		 */
 		max?: number;
+		/**
+		 * The minimal value of the statistic that can take the stats
+		 */
 		min?: number;
+		/**
+		 * The combinaison that can be made with ANOTHER statistic
+		 * Automatically disable the max/min value
+		 */
 		combinaison?: string;
 	};
 };
@@ -56,7 +99,17 @@ export interface StatisticalTemplate {
 	};
 }
 
+/**
+ * If the result can be considered as a critical
+ * Critical is compared to the "natural" dice result, so any modifier doesn't count
+ */
 export interface Critical {
+	/**
+	 * The value that will be considered as a success
+	 */
 	success?: number;
+	/**
+	 * The value that will be considered as a failure
+	 */
 	failure?: number;
 }
