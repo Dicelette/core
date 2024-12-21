b00563c97e95ae525ffaec6167fbeb35bf9979a2
Mara-Li <lili.simonetti@outlook.fr>
2024-12-21 01:46:20 +0100

    fix(utils): improve stat replacement
    - Introduce `standardizeDice` function to handle text within brackets while standardizing non-bracketed text
    - Update regex in stat replacement to exclude text within brackets
    - Remove unnecessary standardization of dice in `replaceFormulaInDice` function for clarity and performance improvement

--- src/utils.ts
+++ src/utils.ts
@@ -11,6 +11,19 @@ export function escapeRegex(string: string) {
 }
 
 /**
+ * Allow to keep the text as if in brackets
+ * @param dice {string}
+ * @return {string} the dice with the text in brackets as if, but the dice (not in brackets) is standardized
+ */
+export function standardizeDice(dice: string): string {
+	return dice.replace(
+			/(\[[^\]]+\])|([^[]+)/g,
+			(match, insideBrackets, outsideText) =>
+				insideBrackets ? insideBrackets : outsideText.standardize()
+		);
+}
+
+/**
  * Replace the stat name by their value using stat and after evaluate any formula using `replaceFormulaInDice`
  * @param originalDice {dice}
  * @param stats {Record<string,number>}
@@ -21,14 +34,14 @@ export function generateStatsDice(
 	stats?: Record<string, number>,
 	dollarValue?: string
 ) {
-	let dice = originalDice.standardize();
+	let dice = originalDice;
 	if (stats && Object.keys(stats).length > 0) {
 		//damage field support adding statistic, like : 1d6 + strength
 		//check if the value contains a statistic & calculate if it's okay
 		//the dice will be converted before roll
 		const allStats = Object.keys(stats);
 		for (const stat of allStats) {
-			const regex = new RegExp(escapeRegex(stat.standardize()), "gi");
+			const regex = new RegExp(`(?!\\[)${escapeRegex(stat.standardize())}(?!\\])`, "gi");
 			if (dice.match(regex)) {
 				const statValue = stats[stat];
 				dice = dice.replace(regex, statValue.toString());
@@ -47,8 +60,7 @@ export function replaceFormulaInDice(dice: string) {
 	const formula = /(?<formula>\{{2}(.+?)}{2})/gim;
 	// biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
 	let match;
-	let modifiedDice = dice.standardize();
-	// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+	let modifiedDice = dice;
 	while ((match = formula.exec(dice)) !== null) {
 		if (match.groups?.formula) {
 			const formulae = match.groups.formula.replaceAll("{{", "").replaceAll("}}", "");
