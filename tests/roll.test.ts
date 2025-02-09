88578fdf474d2f751378775d0904c1679c709eef
Mara <mara-li@outlook.fr>
2025-02-09 11:00:49 +0100

    test: combine test + add describe for better reading

--- tests/roll.test.ts
+++ tests/roll.test.ts
@@ -1,4 +1,4 @@
-import { expect, it } from "bun:test";
+import { describe, expect, it } from "bun:test";
 import * as core from "../src";
 
 it("should roll the dice correctly", () => {
@@ -30,13 +30,57 @@ it("should roll the dice correctly with multiple dice", () => {
 	expect(result!.total).toBeLessThanOrEqual(24);
 });
 
-it("should allow to keep the result in a dice with &", () => {
-	const result = core.roll("2d6;&+2");
-	expect(result).not.toBeUndefined();
-	expect(result!.dice).toEqual("2d6");
+describe("Shared results", () => {
+	it("should allow to keep the result in a dice with &", () => {
+		const result = core.roll("2d6;&+2");
+		expect(result).not.toBeUndefined();
+		expect(result!.dice).toEqual("2d6");
+	});
+	it("should allow to keep the result in a dice with & and comparison", () => {
+		const result = core.roll("2d6;&+2>5");
+		expect(result).not.toBeUndefined();
+		expect(result!.dice).toEqual("2d6");
+	});
 });
-it("should allow to keep the result in a dice with & and comparison", () => {
-	const result = core.roll("2d6;&+2>5");
-	expect(result).not.toBeUndefined();
-	expect(result!.dice).toEqual("2d6");
+
+describe("Test with formula", () => {
+	it("simple roll", () => {
+		const dice = "1d20+$>20";
+		const userStat = 10;
+		const calculation = core.replaceFormulaInDice(
+			dice.replaceAll("$", userStat.toString())
+		);
+		const formula = `${calculation} coucou`;
+		const expectedFormula = "1d20+10>20 coucou";
+		expect(formula).toEqual(expectedFormula);
+	});
+	it("success formula", () => {
+		const dice = "1d20+5>{{$*2}}";
+		const userStat = 10;
+		const calculation = core.replaceFormulaInDice(
+			dice.replaceAll("$", userStat.toString())
+		);
+		const formula = `${calculation} coucou`;
+		const expectedFormula = "1d20+5>20 coucou";
+		expect(formula).toEqual(expectedFormula);
+	});
+	it("complicated formula", () => {
+		const dice = "1d20+{{ceil((10-$)/2)}}>20";
+		const userStat = 5;
+		const calculation = core.replaceFormulaInDice(
+			dice.replaceAll("$", userStat.toString())
+		);
+		const formula = `${calculation} coucou`;
+		const expectedFormula = "1d20+3>20 coucou";
+		expect(formula).toEqual(expectedFormula);
+	});
+	it("negative formula", () => {
+		const dice = "1d20+{{ceil(($-10)/2)}}>20";
+		const userStat = 5;
+		const calculation = core.replaceFormulaInDice(
+			dice.replaceAll("$", userStat.toString())
+		);
+		const expectedFormula = "1d20-2>20";
+		expect(calculation).toEqual(expectedFormula);
+	});
 });
